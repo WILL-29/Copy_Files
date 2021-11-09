d340129f64e064d83012c6ecad7506e00e44b48b
WILL-29 <77632837+WILL-29@users.noreply.github.com>
2021-11-09 13:18:53 -0400

    NA

--- Copy_Files/Form1.Designer.cs
+++ Copy_Files/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(120, 166);
+            this.button1.Location = new System.Drawing.Point(114, 148);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 45);
             this.button1.TabIndex = 0;
